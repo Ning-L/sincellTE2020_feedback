ac8eaa0c71538e2b41bac22f5ae9d639ccc3fb46
Lijiao Ning <lijiao.ning91@gmail.com>
2020-10-19 17:03:36 +0200

    add img and css files

---
+++ libs/remark-img.js
@@ -0,0 +1,9 @@
+remark.macros.scale = function (percentage) {
+  var url = this;
+  return '<img src="' + url + '" style="width: ' + percentage + '" />';
+};
+
+remark.macros.img = function (altText, width) {
+  var url = this;
+  return '<img alt="' + altText + '" src="' + url + '" style="width: ' + width + '" />';
+};
